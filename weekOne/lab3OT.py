2cf730e4dcdd842d4e2d55bbbe1f552adee5b45b
Ousman-T <otouray14@gmail.com>
2025-09-11 18:34:44 -0400

    lab3 OT, formatting

--- weekOne/lab3OT.py
+++ weekOne/lab3OT.py
@@ -1,8 +1,13 @@
 #lab 3 Ousman Touray
 
+print("Example of iterating over an array and printing each index")
 for i in range(5):
     print(i)
+print()
+print("Example of iterating over an array and printing each index using two arguments")
 for k in range(1, 5):
     print(k)
+print()
+print("Example of iterating over an array and printing each index using three arguments")
 for x in range(10,36,5):
     print(x)
