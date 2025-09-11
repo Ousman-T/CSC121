274275b92559e478500e2d9c7ae94c12262f9bc8
Ousman-T <otouray14@gmail.com>
2025-09-11 18:46:23 -0400

    lab3 OT, formatting, added extra assignments

--- weekOne/lab3OT.py
+++ weekOne/lab3OT.py
@@ -1,5 +1,7 @@
 #lab 3 Ousman Touray
 
+#TODO First argument is the start, second is the stop, third is the increment/decriment
+
 print("Example of iterating over an array and printing each index")
 for i in range(5):
     print(i)
@@ -11,3 +13,25 @@ print()
 print("Example of iterating over an array and printing each index using three arguments")
 for x in range(10,36,5):
     print(x)
+print()
+
+for int in range(1, 8):
+    print(int)
+print()
+
+for int in range(40, 46):
+    print(int)
+print()
+
+for int in range(11, 4, -1):
+    print(int)
+print()
+
+for int in range(3, -4, -1):
+    print(int)
+print()
+
+for int in range(100, -1, -10):
+    print(int)
+print()
+
